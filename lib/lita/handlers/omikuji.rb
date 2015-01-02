1005bc5cffbb07275f264d68f177e4259c609062
gouf <innocent.zero@gmail.com>
2015-01-03 06:17:49 +0900

    Initial Commit

---
+++ lib/lita/handlers/omikuji.rb
@@ -0,0 +1,19 @@
+module Lita
+  module Handlers
+    class Omikuji < Handler
+      route(/omikuji/i, :omikuji)
+
+      def omikuji(response)
+        omikuji_list = %w(
+          大吉 吉 中吉 小吉
+          半吉 末吉 末小吉
+          平凶 小凶 半凶
+          末凶 凶 大凶
+        ).freeze
+        response.reply(omikuji_list.sample)
+      end
+    end
+
+    Lita.register_handler(Omikuji)
+  end
+end
